ab4f63373b28d3e4d3c604145004e2a4f9e84f95
Alexandr <alexandrawdxy@gmail.com>
2019-10-06 21:22:44 +0300

    finished todo app

---
+++ todoApp/precache-manifest.7dc46aecca1791a1d8b41138a3fe3978.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a4e1eb6efaa15cbc8f71d09e74e7d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec88813a5caad5d71c32",
+    "url": "/static/css/main.59b49097.chunk.css"
+  },
+  {
+    "revision": "a8c5f68d8a54dff8e534",
+    "url": "/static/js/2.ac9032f0.chunk.js"
+  },
+  {
+    "revision": "ec88813a5caad5d71c32",
+    "url": "/static/js/main.7399cad6.chunk.js"
+  },
+  {
+    "revision": "1d13f4823db3a0656d4c",
+    "url": "/static/js/runtime-main.2182a895.js"
+  }
+]);
